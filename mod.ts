c4f5caf3bec542e1ffa6b923c888f891d06316f6
Omochice <mochice.mls.ntl@gmail.com>
2023-09-29 11:07:39 +0900

    style: deno fmt

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 import type { Denops } from "https://deno.land/x/denops_std@v3.0.0/mod.ts";
-import { v4 } from "npm:uuid@7.0.0"
+import { v4 } from "npm:uuid@7.0.0";
 
-console.log("hi", v4())
+console.log("hi", v4());
