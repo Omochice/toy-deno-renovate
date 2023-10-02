4dbe1bac271df6f21dd281932a52610d8bfffd8b
Omochice <mochice.mls.ntl@gmail.com>
2023-10-02 11:52:33 +0900

    chore: add import from github-release

--- mod.ts
+++ mod.ts
@@ -1,4 +1,6 @@
 import type { Denops } from "https://deno.land/x/denops_std@v3.0.0/mod.ts";
 import { v4 } from "npm:uuid@9.0.1";
+import { execPodium } from "https://pax.deno.dev/Omochice/podeno@v1.0.0/src/lua.ts"
+import { indent, dedent } from "https://raw.githubusercontent.com/Omochice/podeno/v1.0.0/src/indent.ts"
 
 console.log("hi", v4());
