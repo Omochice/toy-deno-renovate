dfb308e434efb6e0c2ed828c338958dfa0605242
Omochice <mochice.mls.ntl@gmail.com>
2023-10-31 14:54:44 +0900

    feat: add nest.land

--- mod.ts
+++ mod.ts
@@ -8,6 +8,8 @@ import {
 import { add } from "https://pax.deno.dev/Omochice-toybox/toy-release-please@v1.0.0";
 import { is } from "https://pax.deno.dev/lambdalisue/deno-unknownutil@v3.9.0";
 import { assert } from "https://raw.githubusercontent.com/lambdalisue/deno-unknownutil/v3.9.0/mod.ts";
+import SysTray from "https://x.nest.land/systray@v0.2.0/mod.ts"
+
 
 console.log("hi", v4());
 
