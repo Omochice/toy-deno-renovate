091530b90fa9cffd0f70a1e44feff7e0becfb097
Omochice <mochice.mls.ntl@gmail.com>
2024-05-27 00:05:14 +0900

    chore: update

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import * as Path from "https://deno.land/std@0.224.0/path/mod.ts";
 import * as Fs from "https://deno.land/std@0.224.0/fs/mod.ts";
 import { join } from "jsr:@std/path@0.224.0/join";
+import { join } from "https://jsr.io/@std/path/0.224.0/join.ts";
 
 // console.log("hi", v4());
 //
